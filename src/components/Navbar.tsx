fd75f594207b2b1ff148eab53de3594564cbde24
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-07-21 16:07:49 +0500

    Add 3d feature in website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,14 +67,14 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RH
+          NS
         </a>
         <a
-          href="mailto:redoyanul1234@gmail.com"
+          href="mailto:noumansajid623@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          redoyanul1234@gmail.com
+          noumansajid623@gmail.com
         </a>
         <ul>
           <li>
